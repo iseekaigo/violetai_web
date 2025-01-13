d66aeb27e161cde14fbb4830c923bc4b246e741e
kingfariz <kungfupanda22@gmail.com>
2025-01-14 06:20:39 +0700

    chore: fix copywriting

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -1,7 +1,7 @@
 const Version = () => {
   return (
-    <section className="bg-pink-light py-5">
-      <h2 className="text-xl font-medium text-violet-dark text-center">
+    <section className="bg-pink-light py-1">
+      <h2 className="text-sm text-violet-dark text-center">
         VioletAI v1.0.0 (01/18/2025) | Website v1.0.0 (01/18/2025)
       </h2>
     </section>
